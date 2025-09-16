f5a24dc2c4c6d226d0276bec66c8e6290a35d4c5
Tisa_401 <bliissty73@gmail.com>
2025-09-16 14:19:43 +0700

    inisiasi class Dosen katanya

---
+++ Dosen.java
@@ -0,0 +1,15 @@
+package
+
+public class Dosen{
+    string nama;
+    string nidn;
+
+    void mengampumatakuliah(){
+        Sout.println("Dosen mengampu mata kuliah");
+
+    }
+    void mengisi presensi mahasiswa(){
+        sout.println("Dosen mengisi presensi mahasiswa");
+    }
+
+}
