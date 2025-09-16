b626a7acbf482f5f3680660e29b5ff832578a1fa
AzlinNikenO <azlinniken@gmail.com>
2025-09-16 14:23:17 +0700

    inisialisasi class MataKuliah

---
+++ MataKuliah.java
@@ -0,0 +1,11 @@
+public class MataKuliah {
+    String kodeMataKulliah;
+    String namaMataKuliah;
+    int sks;
+    int semester;
+    String dosenPengampu;
+}
+
+void tampilInfo(){
+    System.out.println("Kode: ");
+}
